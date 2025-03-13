7648b13fb9d5286527efb2c469056bc306f6750b
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2025-03-13 08:07:47 +0800

    Create sample1.cpp

---
+++ Module8/sample1.cpp
@@ -0,0 +1,19 @@
+//regular fibo
+
+#include <iostream>
+using namespace std;
+
+int fibonacciRecursive(int n) {
+    if (n == 0) return 0; // Base case
+    if (n == 1) return 1; // Base case
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2); // Recursive case
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    cout << "Fibonacci number at position " << n << " is: " << fibonacciRecursive(n) << endl;
+    return 0;
+}
